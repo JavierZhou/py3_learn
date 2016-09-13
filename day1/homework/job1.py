94820f1a0d8429a9d633e6095e7e3099f080bfdf
javier <javier.zhou@foxmail.com>
2016-09-13 11:03:06 +0800

    job1 is released job1_v0.1

--- day1/homework/job1.py
+++ day1/homework/job1.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-user = 'javier'
-passwd = '123456'
 black_list = 'black_user'
+count = 0
+user_db = {
+    'javier': '123456',
+    'zjw': '123'
+}
 
 
 def login_check(user):
@@ -18,5 +21,18 @@ def add_black_user(user):
         f.writelines(user)
 
 
-username = input('请输入用户名：')
-password = input('请输入密码：')
+while count < 3:
+    username = input('请输入用户名：')
+    password = input('请输入密码：')
+    if login_check(username):
+        print('账户已被锁定！')
+        break
+    elif username in user_db and password == user_db[username]:
+        print('登陆成功！')
+        break
+    else:
+        print('账户或密码错误！')
+        count += 1
+else:
+    print('尝试次数过多！')
+    add_black_user(username)
